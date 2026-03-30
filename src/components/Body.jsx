3dff45a087930575b9c693f3b942e9ac5c1862da
Toto2807 <146028794+Toto2807@users.noreply.github.com>
2026-03-30 14:31:25 +0200

    add advert

---
+++ src/components/Body.jsx
@@ -0,0 +1,9 @@
+function Body(){
+    return(
+        <div className="avertissement">
+            <h2 className="text-4xl text-center">Ce site est encore en cours de développement d'autre projet vont être importés.</h2>
+        </div>
+    )
+}
+
+export default Body;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,12 @@
 import Presentation from "../components/Presentation.jsx";
+import Body from "../components/Body.jsx";
 import Card from "../components/Card.jsx";
 
 function Content(){
     return(
         <>
             <Presentation />
+            <Body />
             <Card />
         </>
     )
